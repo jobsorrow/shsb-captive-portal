87d9b4aaf47bc06ebb5665d583ab0f7ffc0fbb66
job <nataphonwaxy@gmail.com>
2020-11-03 16:22:01 +0000

    update

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
+from iwlist import scan, parse
+
+
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/', methods=['POST', 'GET'])
 def index():
-	return render_template('index.html')
-
+	if request.method == "GET":
+		return render_template('index.html',wifiscan=parse(scan()))
+	else:
+		ssid = request.form['ssid']
+		pw = request.form['pw']
+		conf = open('/home/pi/conf.conf','a')
+		print(ssid,pw)
+		conf.write('network={ssid=\"'+ssid+'\"psk=\"'+pw+'\"}')
+		conf.close()
+		return render_template('saved.html')
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True,host='0.0.0.0', port=80)

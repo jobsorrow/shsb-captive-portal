8470f00f0cbb7f1a635dbb13a2ba47fb6771ca97
job <nataphonwaxy@gmail.com>
2020-11-05 01:54:26 +0700

    add win support and center essid text

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, request
 from iwlist import scan, parse
-
+win = 1
 
 
 app = Flask(__name__)
@@ -9,11 +9,18 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
 	if request.method == "GET":
-		return render_template('index.html',wifiscan=parse(scan()))
+		if win:
+			ret = open("C:\\Users\\61301765\\Documents\\shsb-pi\\wifiscan_result.txt").read()
+		else :
+			ret = scan()
+		return render_template('index.html',wifiscan=parse(ret))
 	else:
 		ssid = request.form['ssid']
 		pw = request.form['pw']
-		conf = open('/home/pi/conf.conf','a')
+		if  not win:
+			conf = open('/home/pi/conf.conf','a')
+		else :
+			conf = open("C:\\Users\\61301765\\Documents\\shsb-captive-portal\\wpa-conf.txt",'a')
 		print(ssid,pw)
 		conf.write('network={ssid=\"'+ssid+'\"psk=\"'+pw+'\"}')
 		conf.close()
